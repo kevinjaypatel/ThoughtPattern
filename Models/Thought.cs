81a19958769f4de3ae2cf3117f05363fd1721d1e
Kevin Patel <kevinjaypatel@gmail.com>
2020-06-06 13:45:16 -0700

    Added a models folder which contains a model for a Thought

---
+++ Models/Thought.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ThoughtPattern.Models
+{
+    public class Thought
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime ReleaseDate { get; set; }
+    }
+}
+
