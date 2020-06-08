832c0bff6fc4af0804378fa37687dd2128a61be8
Kevin Patel <kevinjaypatel@gmail.com>
2020-06-07 19:59:06 -0700

    Added search capabilities. Filtered Thoughts in descending order

--- Pages/Thoughts/Index.cshtml.cs
+++ Pages/Thoughts/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using ThoughtPattern.Data;
 using ThoughtPattern.Models;
@@ -21,9 +22,24 @@ namespace ThoughtPattern.Pages.Thoughts
 
         public IList<Thought> Thought { get;set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList FilteredThoughts { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string ThoughtDescription { get; set; }
+
         public async Task OnGetAsync()
         {
-            Thought = await _context.Thought.ToListAsync();
+            var thoughts = from t in _context.Thought
+                           orderby t.ReleaseDate descending
+                           select t;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                thoughts = (IOrderedQueryable<Thought>)thoughts.Where(t => t.Description.Contains(SearchString));
+            }
+
+            Thought = await thoughts.ToListAsync();
         }
     }
 }
